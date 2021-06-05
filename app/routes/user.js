903e91f3af1b413303ca9603c17473f71388f216
Jeremias Santos <jeremiassnts3@gmail.com>
2021-06-05 16:17:31 -0300

    fix: edit put

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,6 @@
 module.exports = (context) => {
     context.post('/user/login', (req, res) => context.controllers.user.login(context, req, res))
     context.post('/user/criarusuario', (req, res) => context.controllers.user.criarUsuario(context, req, res))
-    context.post('/user/editarusuario', context.services.auth.verifyJwt, (req, res) => context.controllers.user.editarUsuario(context, req, res))
+    
+    context.put('/user/editarusuario', context.services.auth.verifyJwt, (req, res) => context.controllers.user.editarUsuario(context, req, res))
 }
--- app/services/auth.js
+++ app/services/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 var verifyJwt = (req, res, next) => {
-    const token = req.headers.authorization.replace("Bearer", "").trim()
+    const token = req.headers.authorization ? req.headers.authorization.replace("Bearer", "").trim() : null
     if (!token) {
         res.status(401).json({ error: true, auth: false, message: 'Nenhum token enviado.' });
     } else {
