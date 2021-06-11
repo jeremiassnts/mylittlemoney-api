96a793a6e1e9fa542fd1ff13aaec33f0f17b8592
Jeremias Santos <jeremiassnts3@gmail.com>
2021-06-11 14:56:44 -0300

    feat: cors

--- config/server.js
+++ config/server.js
@@ -5,6 +5,12 @@ var app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 consign({ cwd: 'app' })
     .include("services")
     .then("controllers")
