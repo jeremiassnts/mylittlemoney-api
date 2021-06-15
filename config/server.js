c44159e8ee39739748187595cce656e1278ebdcf
Jeremias Santos <jeremiassnts3@gmail.com>
2021-06-15 15:25:28 -0300

    feat: cors para authorization

--- config/server.js
+++ config/server.js
@@ -5,17 +5,17 @@ var app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "OPTIONS, Accept, Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Header");
+  next();
+});
 
 consign({ cwd: 'app' })
-    .include("services")
-    .then("controllers")
-    .then("models")
-    .then("routes")    
-    .into(app)
+  .include("services")
+  .then("controllers")
+  .then("models")
+  .then("routes")
+  .into(app)
 
 module.exports = app
